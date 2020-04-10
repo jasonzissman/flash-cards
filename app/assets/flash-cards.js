e8cfd6e1e62444d168d97899ba34b80accc2af19
Jason Z <jason.zissman@gmail.com>
2020-04-10 17:02:36 -0500

    adding websocket logic

--- app/assets/flash-cards.js
+++ app/assets/flash-cards.js
@@ -1,2 +1,6 @@
-alert("Hello!");
-// TODO - hit websocket endpoint to start game
+const tenantId = new URLSearchParams(window.location.search).get('tenantId');
+const gameId = new URLSearchParams(window.location.search).get('gameId');
+
+// Websocket against /{tenantId}/games/{gameId}/join
+// var exampleSocket = new WebSocket("wss://www.example.com/socketserver", "protocolOne");
+
