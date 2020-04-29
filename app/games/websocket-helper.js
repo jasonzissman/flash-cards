93f54ea72db7f215f4066d5d2ec1e59e0bae36e9
Jason Z <jason.zissman@gmail.com>
2020-04-29 10:46:07 -0500

    Moving WS code into helper

---
+++ app/games/websocket-helper.js
@@ -0,0 +1,176 @@
+const WebSocket = require('ws');
+const gameHelper = require("./game-helper");
+const logger = require('./log-helper');
+const { v4: uuidv4 } = require('uuid');
+
+// TODO - run through back end and make sure null-pointer exceptions cannot take down app.
+// THIS INCLUDES SOMEONE SENDING WS COMMANDS AGAINST NON-EXISTENT GAMES OR USERS OR TENANTS
+
+let webSocketHelper = {
+
+    webSocketServer: new WebSocket.Server({
+        port: 3002
+    }),
+
+    startWebSocketServer: () => {
+
+        webSocketHelper.webSocketServer.on('connection', (webSocketConn) => {
+
+            logger.info('connection established');
+
+            webSocketConn.isAlive = true;
+
+            webSocketConn.on('message', (messageStr) => {
+
+                if (!webSocketHelper.isValidMessage(messageStr)) {
+                    logger.error("Invalid message!!! Size: " + messageStr.length);
+                    let message = {
+                        gameType: "FLASH_CARDS_MULTIPLICATION",
+                        messageType: "INVALID_MESSAGE_PROVIDED",
+                        message: "Invalid message provided."
+                    };
+                    webSocketConn.send(JSON.stringify(message));
+
+                } else {
+
+                    let message = JSON.parse(messageStr);
+                    message.action = message.action.replace(/[^a-z-_0-9]+/gi, " ").substring(0, 100).trim();
+
+                    if (message.action === "INITIALIZE_CONNECTION") {
+                        webSocketHelper.initializeWebSocketConnection(webSocketConn, message);
+                    } else if (message.action === "PONG") {
+                        // PING-PONG heartbeat to confirm connection is still alive
+                        webSocketConn.isAlive = true;
+                    } else {
+                        const gameId = webSocketConn.sessionInfo.gameId;
+                        const tenantId = webSocketConn.sessionInfo.tenantId;
+                        const userId = webSocketConn.sessionInfo.userId;
+                        let response = gameHelper.processMessage(gameId, tenantId, userId, message);
+                        webSocketConn.send(JSON.stringify(response));
+                    }
+                }
+            });
+
+            webSocketConn.on('close', () => {
+
+                if (webSocketConn.sessionInfo) {
+                    const gameId = webSocketConn.sessionInfo.gameId;
+                    const tenantId = webSocketConn.sessionInfo.tenantId;
+                    const userId = webSocketConn.sessionInfo.userId;
+
+                    let response = gameHelper.exitGame(gameId, tenantId, userId);
+
+                    let game = gameHelper.getGame(gameId);
+                    if (game) {
+                        game.gameState.gameStateChangeEmitter.removeListener('game-state-changed', webSocketConn.sendGameStateChange);
+                        const userSpecificEventName = 'notify-user-' + userId;
+                        game.gameState.gameStateChangeEmitter.removeAllListeners(userSpecificEventName);
+
+                        if (game.activePlayers.length === 0) {
+                            gameHelper.deleteGame(gameId);
+                        }
+                    }
+
+                    webSocketConn.send(JSON.stringify(response));
+                }
+            });
+        });
+
+        const pingAllConnectionsInterval = setInterval(() => {
+            webSocketHelper.webSocketServer.clients.forEach((webSocketConn) => {
+                if (webSocketConn.isAlive === false) {
+                    return webSocketConn.terminate();
+                }
+                webSocketConn.isAlive = false;
+                webSocketConn.send(JSON.stringify({
+                    gameType: "FLASH_CARDS_MULTIPLICATION",
+                    messageType: "PING",
+                }));
+            });
+        }, 30000);
+
+        webSocketHelper.webSocketServer.on('close', () => {
+            clearInterval(pingAllConnectionsInterval);
+        });
+    },
+
+    isValidMessage: (messageStr) => {
+        let isValid = messageStr && messageStr.length < 1000;
+
+        if (isValid) {
+            try {
+                JSON.parse(messageStr);
+            } catch (e) {
+                logger.info("Improper JSON provided!");
+                isValid = false;
+            }
+        }
+
+        return isValid;
+    },
+
+    getCleansedSessionInfo: (message) => {
+
+        let tenantId = message.tenantId;
+        if (!tenantId) {
+            tenantId = "GENERIC_TENANT_ID";
+        } else {
+            tenantId = tenantId.replace(/[^a-z-_0-9]+/gi, " ").substring(0, 100).trim();
+        }
+
+        let gameId = message.gameId;
+        if (!gameId) {
+            gameId = gameHelper.createNewGame(tenantId);
+        } else {
+            gameId = gameId.replace(/[^a-z-_0-9]+/gi, " ").substring(0, 100).trim();
+        }
+
+        let userId = message.userId;
+        if (!userId) {
+            userId = uuidv4();
+        } else {
+            userId = userId.replace(/[^a-z-_0-9]+/gi, " ").substring(0, 100).trim();
+        }
+
+        return {
+            tenantId: tenantId,
+            gameId: gameId,
+            userId: userId
+        };
+    },
+
+    initializeWebSocketConnection: (webSocketConn, message) => {
+
+        webSocketConn.sessionInfo = webSocketHelper.getCleansedSessionInfo(message);
+
+        webSocketConn.sendGameStateChange = (gameState) => {
+            webSocketConn.send(JSON.stringify({
+                gameType: "FLASH_CARDS_MULTIPLICATION",
+                messageType: "GAME_STATE_CHANGE",
+                activePlayers: gameHelper.getGame(webSocketConn.sessionInfo.gameId).activePlayers,
+                gameState: gameState
+            }));
+        };
+        gameHelper.getGame(webSocketConn.sessionInfo.gameId).gameState.gameStateChangeEmitter.on('game-state-changed', webSocketConn.sendGameStateChange);
+
+        webSocketConn.sendUserNotification = (notification) => {
+            webSocketConn.send(JSON.stringify({
+                gameType: "FLASH_CARDS_MULTIPLICATION",
+                messageType: "NOTIFY_USER",
+                notification: notification
+            }));
+        };
+        const userSpecificEventName = 'notify-user-' + webSocketConn.sessionInfo.userId;
+        gameHelper.getGame(webSocketConn.sessionInfo.gameId).gameState.gameStateChangeEmitter.on(userSpecificEventName, webSocketConn.sendUserNotification);
+
+        webSocketConn.send(JSON.stringify({
+            gameType: "FLASH_CARDS_MULTIPLICATION",
+            messageType: "INIT_CONNECTION_COMPLETE",
+            tenantId: webSocketConn.sessionInfo.tenantId,
+            gameId: webSocketConn.sessionInfo.gameId,
+            userId: webSocketConn.sessionInfo.userId
+        }));
+    }
+};
+
+module.exports = webSocketHelper;
--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const WebSocket = require('ws');
-const { v4: uuidv4 } = require('uuid');
 const logger = require('./app/games/log-helper');
+const WebSocketHelper = require('./app/games/websocket-helper');
+const gameHelper = require("./app/games/game-helper");
 const app = express();
 const httpPort = 3001;
-const webSocketPort = 3002;
-
-// TODO - run through back end and make sure null-pointer exceptions cannot take down app.
-// THIS INCLUDES SOMEONE SENDING WS COMMANDS AGAINST NON-EXISTENT GAMES OR USERS OR TENANTS
-
-const gameHelper = require("./app/games/game-helper");
 
 // HTML/JS
 app.use(express.static('ui'));
@@ -21,159 +15,7 @@ app.get('/:tenantId/active-games', (req, res) => {
   res.status(200).json(activeGamesForTenant);
 });
 
-let initializeConnection = (message, webSocketConn) => {
-
-  let tenantId = message.tenantId;
-  if (!tenantId) {
-    tenantId = "GENERIC_TENANT_ID";
-  } else {
-    tenantId = tenantId.replace(/[^a-z-_0-9]+/gi, " ").substring(0, 100).trim();
-  }
-
-  let gameId = message.gameId;
-  if (!gameId) {
-    gameId = gameHelper.createNewGame(tenantId);
-  } else {
-    gameId = gameId.replace(/[^a-z-_0-9]+/gi, " ").substring(0, 100).trim();
-  }
-
-  let userId = message.userId;
-  if (!userId) {
-    userId = uuidv4();
-  } else {
-    userId = userId.replace(/[^a-z-_0-9]+/gi, " ").substring(0, 100).trim();
-  }
-
-  webSocketConn.sessionInfo = {
-    tenantId: tenantId,
-    gameId: gameId,
-    userId: userId
-  };
-
-  webSocketConn.sendGameStateChange = (gameState) => {
-    webSocketConn.send(JSON.stringify({
-      gameType: "FLASH_CARDS_MULTIPLICATION",
-      messageType: "GAME_STATE_CHANGE",
-      activePlayers: gameHelper.getGame(gameId).activePlayers,
-      gameState: gameState
-    }));
-  };
-  gameHelper.getGame(gameId).gameState.gameStateChangeEmitter.on('game-state-changed', webSocketConn.sendGameStateChange);
-
-  webSocketConn.sendUserNotification = (notification) => {
-    let message = {
-      gameType: "FLASH_CARDS_MULTIPLICATION",
-      messageType: "NOTIFY_USER",
-      notification: notification
-    }
-    webSocketConn.send(JSON.stringify(message));
-  };
-  const userSpecificEventName = 'notify-user-' + userId;
-  gameHelper.getGame(gameId).gameState.gameStateChangeEmitter.on(userSpecificEventName, webSocketConn.sendUserNotification);
-
-  webSocketConn.send(JSON.stringify({
-    gameType: "FLASH_CARDS_MULTIPLICATION",
-    messageType: "INIT_CONNECTION_COMPLETE",
-    tenantId: webSocketConn.sessionInfo.tenantId,
-    gameId: webSocketConn.sessionInfo.gameId,
-    userId: webSocketConn.sessionInfo.userId
-  }));
-};
-
-function isValidRequest(messageStr) {
-  let isValid = messageStr && messageStr.length < 1000;
-
-  if (isValid) {
-    try {
-      JSON.parse(messageStr);
-    } catch (e) {
-      logger.info("Improper JSON provided!");
-      isValid = false;
-    }
-  }
-
-  return isValid;
-}
-
-// Handle websocket connection
-const webSocketServer = new WebSocket.Server({ port: webSocketPort });
-webSocketServer.on('connection', (webSocketConn) => {
-
-  logger.info('connection established');
-
-  webSocketConn.isAlive = true;
-
-  webSocketConn.on('message', (messageStr) => {
-
-    if (!isValidRequest(messageStr)) {
-      logger.error("Invalid message!!! Size: " + messageStr.length);
-      let message = {
-        gameType: "FLASH_CARDS_MULTIPLICATION",
-        messageType: "INVALID_MESSAGE_PROVIDED",
-        message: "Invalid message provided."
-      };
-      webSocketConn.send(JSON.stringify(message));
-
-    } else {
-
-      let message = JSON.parse(messageStr);
-      message.action = message.action.replace(/[^a-z-_0-9]+/gi, " ").substring(0, 100).trim();
-
-      if (message.action === "INITIALIZE_CONNECTION") {
-        initializeConnection(message, webSocketConn);
-      } else if (message.action === "PONG") {
-        // PING-PONG heartbeat to confirm connection is still alive
-        webSocketConn.isAlive = true;
-      } else {
-        const gameId = webSocketConn.sessionInfo.gameId;
-        const tenantId = webSocketConn.sessionInfo.tenantId;
-        const userId = webSocketConn.sessionInfo.userId;
-        let response = gameHelper.processMessage(gameId, tenantId, userId, message);
-        webSocketConn.send(JSON.stringify(response));
-      }
-    }
-  });
-
-  webSocketConn.on('close', () => {
-
-    if (webSocketConn.sessionInfo) {
-      const gameId = webSocketConn.sessionInfo.gameId;
-      const tenantId = webSocketConn.sessionInfo.tenantId;
-      const userId = webSocketConn.sessionInfo.userId;
-
-      let response = gameHelper.exitGame(gameId, tenantId, userId);
-
-      let game = gameHelper.getGame(gameId);
-      if (game) {
-        game.gameState.gameStateChangeEmitter.removeListener('game-state-changed', webSocketConn.sendGameStateChange);
-        const userSpecificEventName = 'notify-user-' + userId;
-        game.gameState.gameStateChangeEmitter.removeAllListeners(userSpecificEventName);
-
-        if (game.activePlayers.length === 0) {
-          gameHelper.deleteGame(gameId);
-        }
-      }
-
-      webSocketConn.send(JSON.stringify(response));
-    }
-  });
-});
-
-const pingAllConnectionsInterval = setInterval(() => {
-  webSocketServer.clients.forEach((webSocketConn) => {
-    if (webSocketConn.isAlive === false) {
-      return webSocketConn.terminate();
-    }
-    webSocketConn.isAlive = false;
-    webSocketConn.send(JSON.stringify({
-      gameType: "FLASH_CARDS_MULTIPLICATION",
-      messageType: "PING",
-    }));  });
-}, 30000);
-
-webSocketServer.on('close', () => {
-  clearInterval(pingAllConnectionsInterval);
-});
+WebSocketHelper.startWebSocketServer();
 
 app.listen(httpPort, () => {
   logger.info('Flashcards app listening on port %s!', httpPort);
